491decea4c4ec383015180033c0096e89ee433c8
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2025-02-11 16:33:54 -0300

    Add console.sol no contract

--- Smart-Contracts/turing_dapp/contracts/Token.sol
+++ Smart-Contracts/turing_dapp/contracts/Token.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract Token is ERC20, Ownable {
     mapping(string => address) private codinomes;
